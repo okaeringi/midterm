9206732a9ee0c3abb83400ce9ddce9e2e661daba
okaeringi <eriokawaeri@gmail.com>
2014-06-05 11:22:02 +0900

    99
    99

--- 09/factorial.js
+++ 09/factorial.js
@@ -1,18 +1,28 @@
 function factorial(num){
 		if(num>=1){
+			result1 = 1;
 			for(i=1;i<=num;i++){
-				i*=i;
+				result1=result1*i;
 			}
+			return result1;
 		}
 		
-		if(num>0){
+		if(num>=0){
 			Math.floor(num);
+			result2 = 1;
 			for(i=1;i<=num;i++){
-				i*=i;
+				result2=result2*i;
 			}
+			return result2;
 		}
 		
 		if(num<=0){
 			return null;
 		}
+
+
+		if(isNaN(num)=true){
+			return null;
+		}
+
 	}
