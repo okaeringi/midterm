b245e22bc3500f7303b627b645f7c5e4ad1011e4
okaeringi <eriokawaeri@gmail.com>
2014-06-05 11:36:09 +0900

    10
    10

--- 10/prime.js
+++ 10/prime.js
@@ -1,5 +1,9 @@
-Function prime(x){
-If( x >= 0){
-}else{
-}
+function isPrime(n) {
+    if (n%2 == 0) return n == 2;
+    if (n%3 == 0) return n == 3;
+    var limit = Math.sqrt(n);
+    for (var i=5, d=2; i <= limit; i+=d, d=6-d) {
+        if (n%i == 0) return false;
+    }
+    return true;
 }
